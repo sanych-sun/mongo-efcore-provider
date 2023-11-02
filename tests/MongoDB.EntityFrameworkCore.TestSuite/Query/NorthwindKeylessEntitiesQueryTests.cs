60f127eef94ab31bfddc764bba54e3933ee00406
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2023-11-02 12:53:48 -0700

    wip

--- tests/MongoDB.EntityFrameworkCore.TestSuite/Query/NorthwindKeylessEntitiesQueryTests.cs
+++ tests/MongoDB.EntityFrameworkCore.TestSuite/Query/NorthwindKeylessEntitiesQueryTests.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.TestUtilities;
 
 namespace MongoDB.EntityFrameworkCore.TestSuite.Query
 {
-    public class NorthwindKeylessEntitiesQueryTests : NorthwindSelectQueryTestBase<NorthwindQueryMongoDbFixture<NoopModelCustomizer>>
+    public class NorthwindKeylessEntitiesQueryTests : NorthwindKeylessEntitiesQueryTestBase<NorthwindQueryMongoDbFixture<NoopModelCustomizer>>
     {
         public NorthwindKeylessEntitiesQueryTests(NorthwindQueryMongoDbFixture<NoopModelCustomizer> fixture) : base(fixture)
         {
